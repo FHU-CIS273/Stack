b91e42d5edf08e0d52a8ba27378142f56ebe5d9e
Kenan Casey <kenancasey@gmail.com>
2019-09-25 10:42:17 -0500

    initial commit

---
+++ Stack/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Stack
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Stack<string> stack = new Stack<string>();
+
+            stack.Push("Hulk");
+            stack.Push("Captain America");
+            stack.Push("Black Widow");
+
+            Console.WriteLine( stack.Peek() );  // Black Widow
+
+            Console.WriteLine( stack.Pop() );   // Black Widow
+
+            Console.WriteLine( stack.Pop() );   // Captain America
+
+
+            
+
+        }
+    }
+}
---
+++ Stack/Stack.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Stack
+{
+    public class Stack<T>
+    {
+        private LinkedList<T> linkedList;
+
+        public Stack()
+        {
+            linkedList = new LinkedList<T>();
+
+
+            var last = linkedList.Last;
+
+
+            var enumerator = linkedList.GetEnumerator();
+
+            
+        }
+
+        // add item to top of stack
+        public void Push(T item)
+        {
+            linkedList.AddLast(item);
+        }
+
+        // return top of stack and remove it.
+        public T Pop()
+        {
+            T topOfStack = linkedList.Last.Value;
+
+            linkedList.RemoveLast();
+
+            return topOfStack;
+        }
+
+        // return top of stack.
+        public T Peek()
+        {
+            return linkedList.Last.Value;
+        }
+
+
+        public int Size => linkedList.Count;
+
+    }
+}
